5df2d5403a62d245b90dfaaba9e3098349e48e1a
billbilW <billbilW2025@outlook.com>
2025-05-25 23:21:30 +0800

    Add files via upload
    上传！改了好多次

---
+++ src/main/java/com/example/vipreport/MainClass.java
@@ -0,0 +1,9 @@
+
+package com.example.vipreport;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("VIP Report Application Started");
+        // 添加您的业务逻辑
+    }
+}
