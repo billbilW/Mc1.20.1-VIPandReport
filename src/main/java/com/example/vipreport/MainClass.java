3d6148a2d857768137de9517a8dfeee89cb6da37
billbilW <billbilW2025@outlook.com>
2025-05-29 22:35:26 +0800

    Delete src/main directory

--- src/main/java/com/example/vipreport/MainClass.java
+++
@@ -1,9 +0,0 @@
-
-package com.example.vipreport;
-
-public class MainClass {
-    public static void main(String[] args) {
-        System.out.println("VIP Report Application Started");
-        // 添加您的业务逻辑
-    }
-}
